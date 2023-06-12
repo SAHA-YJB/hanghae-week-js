59fe6ab4d7668d76ac593c5067369bc36575b1d5
SAHA-YJB <pkomom123@naver.com>
2023-06-12 11:51:11 +0900

    불,언디파인드, 널, 객체 배열

---
+++ 04.js
@@ -0,0 +1,30 @@
+// 불타입 true, false(참, 거짓)
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1);
+console.log(bool2);
+console.log(typeof bool1);
+console.log(typeof bool2);
+
+//undefined (할당되지 않은 값)
+let noValue;
+console.log(noValue);
+
+// null (의도적으로 값이 존재하지 않음)
+let noValue2 = null;
+console.log(noValue2);
+
+//object key: value
+let person = {
+  name: "SAHA",
+  age: "1994",
+  test: true,
+};
+console.log(person);
+console.log(typeof person);
+
+// array 여러개의 데이터를 순서대로 저장
+// 제로베이스넘버링 인덱스번호는 0부터 시작!
+let numArray = [1, 2, 3, 4, 5];
+let fruits = ["apple", "banana", "orange"];
