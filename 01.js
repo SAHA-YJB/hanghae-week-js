37751057c3c10b595c4cbba014b2d8e9f1b5caa0
SAHA-YJB <pkomom123@naver.com>
2023-06-12 11:07:58 +0900

    변수 상수

---
+++ 01.js
@@ -0,0 +1,18 @@
+//변수, 상수 -> 메모리 저장 -> 읽어들여 재사용
+// 변수 이름 - 고유이름 = myLet
+// 변수 값 - 저장된 값 = "helloWorld"
+// 변수 할당 - 변수에 저장하는 행위
+// 변수 선언 - 컴퓨터에게 알리는 행위 let mtLet
+// 변수 참조 - 할당된 값을 읽어옴
+// 선언방법 let, const var는 쓰지 말기
+
+let myLet = "hello World";
+console.log(myLet);
+
+const myConst = "hello World"
+console.log(myConst);
+
+// ex)
+let myLet2;
+myLet2 = "helloWorld"
+console.log(myLet2);
