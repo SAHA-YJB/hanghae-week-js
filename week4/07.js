82fb6d98d73a1c63c954e5086abd27af44e8c396
SAHA-YJB <pkomom123@naver.com>
2023-06-14 20:06:43 +0900

    프로미스 리팩토링 연습

---
+++ week4/07.js
@@ -0,0 +1,20 @@
+//프로미스 리팩토링
+
+const addCoffee = function (name) {
+  return function (prevName) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        let newName = prevName 
+                      ? `${prevName}, ${name}` 
+                      : name;
+        console.log(newName);
+        resolve(newName);
+      }, 500);
+    });
+  };
+};
+
+addCoffee("에스프레소")()
+  .then(addCoffee("아메리카노"))
+  .then(addCoffee("카페라떼"))
+  .then(addCoffee("뜨아"));
